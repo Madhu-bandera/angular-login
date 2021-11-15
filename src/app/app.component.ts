9a307ed524468273b51b3cc8e9bd53bc60dc498f
oknizam <oknizam321@gmail.com>
2021-11-15 21:51:05 +0530

    Changes updatded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'login-angular';
+export class AppComponent implements OnInit {
+    signinForm!: FormGroup;
+    loggedIn: boolean = false;
+    user!: SocialUser;
+
+    constructor(
+        private fb: FormBuilder,
+        private authService: SocialAuthService
+    ) { }
+
+    ngOnInit() {
+        this.signinForm = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+
+        this.authService.authState.subscribe((user: any) => {
+            this.user = user;
+            this.loggedIn = (user != null);
+        });
+    }
+    signInWithGoogle(): void {
+        this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    }
+
+    signOut(): void {
+        this.authService.signOut();
+    }
+
+    refreshToken(): void {
+        this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+    GoogleLoginProvider,
+} from 'angularx-social-login';
+import {
+    NbThemeModule, NbCardModule
+
+} from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        SocialLoginModule,
+        ReactiveFormsModule,
+        NbThemeModule.forRoot(),
+        NbButtonModule,
+        NbCardModule,
+        NbLayoutModule,
+        NbSidebarModule.forRoot(),
+        NbThemeModule.forRoot({ name: 'default' }),
+        NbEvaIconsModule
+
+    ],
+    providers: [
+        {
+            provide: 'SocialAuthServiceConfig',
+            useValue: {
+                autoLogin: false,
+                providers: [
+                    {
+                        id: GoogleLoginProvider.PROVIDER_ID,
+                        provider: new GoogleLoginProvider(
+                            '334921080524-7a8irkm9m46ag856bu130hjrithmgg0p.apps.googleusercontent.com'
+                        )
+                    },
+
+                ]
+            } as SocialAuthServiceConfig,
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
